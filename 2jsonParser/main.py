e98e8dd7bb6b543762bc3a8558b4a1fccd99b19b
Divan009 <divadugar@gmail.com>
2023-09-14 11:28:00 +0530

    Step 1 completed

---
+++ 2jsonParser/main.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import argparse
+
+def check_syntax_json(file):
+    try:
+        file = Path(file)
+        # if file.exists:
+        with open(file) as f:
+            char = f.read()
+            try:
+                if char[0] == "{" and char[-1]== "}":
+                    return "Valid Json file"
+            except IndexError:
+                return "Invalid Json file"     
+    except Exception as e:
+        return f"File {str(file).split('/')[-1]} does not exist."
+    
+
+if __name__ == '__main__':
+    msg = "This is a copy of wc"
+    parser = argparse.ArgumentParser(description=msg)
+
+    # Adding optional arguments    
+    parser.add_argument("file_name", nargs='?', default=None, help="the file to analyze")
+    # Read arg from command lineon
+    args = parser.parse_args()
+
+    print(check_syntax_json(args.file_name))
