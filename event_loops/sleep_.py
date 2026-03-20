a9287c9a2406ceb68823e3e672c4e19906076dc7
divan009 <divadugar@gmail.com>
2026-03-20 23:04:10 +0530

    understanding event loop - optimised

--- event_loops/sleep_.py
+++ event_loops/sleep_.py
@@ -2,37 +2,75 @@ import time
 from collections import deque
 import random
 
-sleeping = []
-ready = deque()
-
 
 def schedule_task(task, delay):
     deadline = time.time() + delay
     sleeping.append((deadline, task))
 
 
-total_time = 0
-for _ in range(10):
-    delay_s = random.randint(1, 10)
-    total_time += delay_s
-    schedule_task(lambda: print("Printing after delay"), 5)
+def optimised_el():
+    while ready or sleeping:
+        now = time.time()
+        sleeping.sort(key=lambda x: x[0])
+
+        if not ready:
+
+            deadline, task = sleeping[0]
+            delta = deadline - now
+
+            if delta > 0:  # sleep until its ready
+                time.sleep(delta)
+            else:
+                sleeping.pop()
+                ready.append(task)
+
+        # execute task
+        while ready:
+            task = ready.popleft()
+            task()
 
 
-start_time = time.time()
+def unoptimised_el():
+    while ready or sleeping:
+        if not ready:
+            deadline, task = sleeping.pop()
+            delta = deadline - time.time()
+            if delta > 0:  # check if deadline is over
+                time.sleep(delta)
 
-while ready or sleeping:
-    if not ready:
-        deadline, task = sleeping.pop()
-        delta = deadline - time.time()
-        if delta > 0:  # check if deadline is over
-            time.sleep(delta)
+            ready.append(task)
 
-        ready.append(task)
+        # execute task
+        while ready:
+            task = ready.popleft()
+            task()
 
-    # execute task
-    while ready:
-        task = ready.popleft()
-        task()
 
-print(f"total time taken: {time.time() - start_time}")
-print(f"total time expected: {total_time}s")
+if __name__ == "__main__":
+    sleeping = []
+    ready = deque()
+    random.seed(42)
+    total_time = 0
+
+    for _ in range(10):
+        delay_s = random.randint(1, 10)
+        total_time += delay_s
+        schedule_task(lambda: print("Printing after delay"), delay_s)
+
+    start_time = time.time()
+    unoptimised_el()
+    print("unoptimised", end="\n")
+    print(f"total time taken: {time.time() - start_time}")
+    print(f"total time expected: {total_time}s")
+
+    total_time = 0
+    for _ in range(10):
+        delay_s = random.randint(1, 10)
+        total_time += delay_s
+        schedule_task(lambda: print("Printing after delay"), delay_s)
+
+    print("optimised")
+    start_time = time.time()
+    optimised_el()
+    print(f"total time taken: {time.time() - start_time}")
+    print(f"total time expected: {total_time}s")
